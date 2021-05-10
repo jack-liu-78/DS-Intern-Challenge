82fce53fb072e0a276f1b1373a104f6131856243
Jack Liu <jack.78.liu@gmail.com>
2021-05-09 22:59:01 -0400

    Shopify DS challenge

--- shopify_data_parser.py
+++ shopify_data_parser.py
@@ -5,11 +5,11 @@ FILE_NAME = 'data.csv'
 
 data = pd.read_csv(FILE_NAME)
 
+# filtering out identified outliers
 data = data.loc[data["total_items"] != 2000]
 data = data.loc[data["shop_id"] != 78]
 
-print(data)
-
+# calculating and outputting new mean
 order_amounts = data['order_amount'].to_numpy()
 aov = np.mean(order_amounts)
 print(aov)
